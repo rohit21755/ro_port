a322af8861de5e40b4fa4f31989734357d0c80a4
rohit21755 <77154384+rohit21755@users.noreply.github.com>
2023-08-04 13:58:33 +0530

    Add files via upload

--- src/components/HomeIntro.jsx
+++ src/components/HomeIntro.jsx
@@ -2,17 +2,24 @@ import i1 from '../../public/assets/i1.png';
 // import Image from 'next/image';
 const HomeIntro = () => {
     return(<>
-        <div className="mt-12 px-64">
-            <div className='mx-auto w-[705px] h-[728px]' style={{
+        <div className="mt-12 px-64 text-white " >
+            <div className='mx-auto pt-64 h-[600px]' style={{
                 background: `url(${i1.src})`,
                 backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
+                backgroundPosition:'center center'
+                // backgroundSize: 'cover',
+
                 // height: '741px',
                 // width: '736px',
             }}>
-              
+              <p className='text-5xl w-3/5 text-center tracking-wide text-white mx-auto'>Building the Next Genration of Web3 Solution</p>
             </div>
+            
         </div>
+        {/* <div className='absolute w-full top-[50%] '>
+            <div className='text-white mx-auto'></div>
+            
+        </div> */}
 
     </>)
 }
